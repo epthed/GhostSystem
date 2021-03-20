1440fd8777d7fa6ae10419cd431b2c6dbbd8a762
epthed <epthedemail@gmail.com>
2021-03-19 18:36:05 -0700

    allow non-localhost

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import websocket
 
 
 def main() -> None:
-    websocket.goFast.run(port=os.environ.get('PORT'))  # set your local dev environment
+    websocket.goFast.run(host='0.0.0.0', port=os.environ.get('PORT'))  # set your local dev environment
 
 
 if __name__ == "__main__":
